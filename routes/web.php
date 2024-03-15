f48e07495cd71bb4ad9fd00bcb61bf7a7e8b381d
Hanner <Hannerb48@gmail.com>
2024-03-15 10:48:24 -0500

    Se agregó middleware, y se acomodó controlladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use GuzzleHttp\Middleware;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('register');
 });
+
+Route::view('/login', 'login')->name('login');
+Route::view('/registro', 'register')->name('registro');
+Route::view('/privada', 'secret')->Middleware('auth')->name('privada');
+
+Route::post('/validar-registro', [LoginController::class, 'register'])->name('validar-registro');
+Route::post('/inicia-sesion', [loginController::class, 'login'])->name('inicia-sesion');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
